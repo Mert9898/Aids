d1aa2ecae0376e24fa86aca52e294a97959830bf
Mert98 <smilinggreenfrog31@gmail.com>
2025-02-22 22:58:18 +0300

    3rd commit

--- ProjectAids/app.py
+++ ProjectAids/app.py
@@ -1,7 +1,14 @@
 from flask import Flask, jsonify, render_template
 import threading
 import time
-import psutil
+import subprocess
+
+# Ensure psutil is installed
+try:
+    import psutil
+except ImportError:
+    subprocess.check_call(["python", '-m', 'pip', 'install', 'psutil'])
+    import psutil
 
 app = Flask(__name__)
 open_ports = []
@@ -9,7 +16,10 @@ open_ports = []
 def get_open_ports():
     global open_ports
     while True:
-        open_ports = [conn.laddr.port for conn in psutil.net_connections(kind='inet') if conn.status == 'LISTEN']
+        try:
+            open_ports = [conn.laddr.port for conn in psutil.net_connections(kind='inet') if conn.status == 'LISTEN']
+        except Exception as e:
+            print(f"An error occurred while getting open ports: {e}")
         time.sleep(5)
 
 @app.route('/')
@@ -21,5 +31,10 @@ def api_open_ports():
     return jsonify(open_ports)
 
 if __name__ == "__main__":
-    threading.Thread(target=get_open_ports).start()
+    # Start the background thread to get open ports
+    thread = threading.Thread(target=get_open_ports)
+    thread.daemon = True  # Ensure the thread exits when the main program exits
+    thread.start()
+    
+    # Run the Flask app
     app.run(debug=True)
